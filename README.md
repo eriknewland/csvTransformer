a6c9b52933de4c4440ef9802aac68322c2769ae3
eriknewland <114263701+eriknewland@users.noreply.github.com>
2023-03-12 23:08:35 -0700

    Setup Package

--- README.md
+++ README.md
@@ -1,1 +1,149 @@
-# csvTransformer
+<img src="https://user-images.githubusercontent.com/114263701/220216031-59ab1a4f-f767-4846-993a-9f63219bef0e.svg" height="200" width="1000">
+
+<p align="center">
+  <img alt="monthly downloads" src="https://img.shields.io/npm/dm/csvTransformer?style=for-the-badge">
+  <img alt="monthly git commits" src="https://img.shields.io/github/commit-activity/m/eriknewland/csvTransformer?style=for-the-badge">
+</p>
+
+### Written in
+![TypeScript](https://img.shields.io/static/v1?style=for-the-badge&message=TypeScript&color=3178C6&logo=TypeScript&logoColor=FFFFFF&label=)
+
+### Powered by
+<p align="center">
+  <a href="https://nodejs.org/en/">
+    <img alt="NodeJS" src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white" />
+  </a>
+  <a href="https://www.python.org/shell/">
+    <img alt="python-shell" src="https://img.shields.io/badge/python-shell-red?style=for-the-badge&logo=npm&logoColor=white" />
+  </a>
+  <a href="https://www.python.org/">
+    <img alt="Python" src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=ffdd54" />
+  </a>
+  <a href="https://dask.org/">
+    <img alt="Dask" src="https://img.shields.io/badge/dask-black?style=for-the-badge&logo=dask&logoColor=orange" />
+  </a>
+  <a href="https://pandas.pydata.org/">
+    <img alt="pandas" src="https://img.shields.io/static/v1?style=for-the-badge&message=pandas&color=150458&logo=pandas&logoColor=FFFFFF&label=" />
+  </a>
+  <a href="https://https://pypi.org//">
+     <img alt="PyPi" src="https://img.shields.io/static/v1?style=for-the-badge&message=PyPI&color=3775A9&logo=PyPI&logoColor=FFFFFF&label=" />
+  </a>
+</p>
+
+
+#### The easiest way to transform large csv files in js and ts.
+###### BETA (1.0.0)
+
+csvTransformer is a collection of functions designed to simplify large csv file manipulation in javascript and typescript. Functions are designed to be narrow in scope but extremely easy to use. This library compliments existing csv libraries that struggle with memory constraints of large data manipulation.
+
+Need something custom? Check out .custom()! You can create your own python script using pandas, dask, or any other python library you install.
+
+This library was born out of the need to modify specific column cells within large CSV file, using a very underpowered machine.
+
+*As this project is still in beta, it is highly recommended to make a copy of any file you plan to use with this library.*
+
+### UPDATES
+
+1.0.0 is live 🥳
+
+## TO-DO:
+
+1. refactor to improve DRY coding principles
+
+## Usage
+>npm i csvTransformer
+
+```
+import {csvTransformer} from 'csvTransformer'
+```
+or
+```
+const {csvTransformer} = require('csvTransformer')
+```
+
+2. Install python (python.org)
+
+3. Install python libraries with python package installer (pip3 as of 03/23). Sample installation shown below.
+  - numpy
+  - pandas
+  - dask
+  - datetime
+  - glob
+
+```
+pip3 install numpy
+pip3 install pandas
+pip3 install dask
+pip3 install datetime
+pip3 install glob
+```
+> Note: Library built on nodeJS 16.17.1 and Python 3.11.2
+
+#### .aggregatorBool()
+```javascript
+Coming Soon
+```
+
+#### .aggregatorInt()
+```javascript
+Coming Soon
+```
+
+#### .binaryToBoolean()
+```javascript
+Coming Soon
+```
+
+#### .booleanToBinary()
+```javascript
+Coming Soon
+```
+
+#### .custom()
+```javascript
+Coming Soon
+```
+
+#### .generator()
+```javascript
+Coming Soon
+```
+
+#### .isoToUnix()
+```javascript
+Coming Soon
+```
+
+#### .merge()
+```javascript
+Coming Soon
+```
+
+#### .toLower()
+```javascript
+Coming Soon
+```
+
+#### .unixToISO()
+```javascript
+Coming Soon
+```
+
+## Resources
+
+[NodeJS](https://nodejs.org/dist/latest-v6.x/docs/api/fs.html)
+
+[Python](https://www.python.org/)
+
+[Python-Shell](https://www.npmjs.com/package/python-shell)
+
+[Pandas](https://pandas.pydata.org/)
+
+[Dask](https://www.dask.org/)
+
+[PyPi](https://pypi.org/)
+
+
+# Closing
+
+Special thanks to every mention of transforming CSV files (in js) across the web. Contributions always welcome!
---
+++ src/index.ts
@@ -0,0 +1,351 @@
+import { PythonShell } from 'python-shell';
+import * as fs from 'fs';
+
+export const csvTransformer = {
+  aggregatorBool: (
+    inputFile: string,
+    outputFile?: string,
+    idCol?: string,
+    targetCol?: string,
+    newCol1?: string,
+    newCol2?: string,
+  ): void => {
+    outputFile = outputFile || 'csvTransformer_output.csv';
+    idCol = idCol || 'id';
+    targetCol = targetCol || 'recommend';
+    newCol1 = newCol1 || 'recommend_true';
+    newCol2 = newCol2 || 'recommend_false';
+    let i = 0;
+    setInterval(() => {
+      process.stdout.clearLine(0);
+      process.stdout.cursorTo(0);
+      i = (i + 1) % 4;
+      const dots = new Array(i + 1).join('.');
+      process.stdout.write('\x1b[32m' + `Working${dots}` + '\x1b[0m');
+    }, 300).unref();
+    const script = `
+import pandas as pd
+
+#read csv file
+df = pd.read_csv('${inputFile}')
+
+#aggregate the number of true/false values in the 'recommend' column of each unique 'id'
+df_agg = df.groupby('${idCol}')['${targetCol}'].value_counts().unstack().fillna(0).round(0).astype(int)
+
+#write the count of true/false to 2 new columns
+df_agg.rename(columns={True:'${newCol1}', False:'${newCol2}'}, inplace=True)
+
+#output the file
+df_agg.to_csv('${outputFile}')
+  `;
+
+    PythonShell.runString(script, {})
+      .then(() => {
+        process.stdout.clearLine(0);
+        process.stdout.cursorTo(0);
+        process.stdout.write(
+          '\x1b[32m' + 'Aggregation complete! Thanks for choosing csvTransformer  (ᵔᴥᵔ)' + '\x1b[0m\n',
+        );
+      })
+      .catch((err) => console.log(err));
+  },
+  isoToUnix: (inputFile: string, outputFile?: string, targetCol?: string): void => {
+    outputFile = outputFile || 'csvTransformer_output.csv';
+    targetCol = targetCol || 'date';
+    let i = 0;
+    setInterval(() => {
+      process.stdout.clearLine(0);
+      process.stdout.cursorTo(0);
+      i = (i + 1) % 4;
+      const dots = new Array(i + 1).join('.');
+      process.stdout.write('\x1b[32m' + `Working${dots}` + '\x1b[0m');
+    }, 300).unref();
+    const script = `
+import dask.dataframe as dd
+import datetime
+
+# Read in the CSV file
+df = dd.read_csv('${inputFile}')
+
+# Convert the ISO 8601 date to UNIX timestamp
+df['${targetCol}'] = df['${targetCol}'].apply(lambda x: int(datetime.datetime.strptime(x, '%Y-%m-%dT%H:%M:%SZ').timestamp()), meta=('timestamp', int))
+
+# Write the output to a new CSV file
+df.to_csv('${outputFile}', single_file=True, index=False)
+`;
+
+    PythonShell.runString(script, {})
+      .then(() => {
+        process.stdout.clearLine(0);
+        process.stdout.cursorTo(0);
+        process.stdout.write(
+          '\x1b[32m' + 'ISO-->UNIX Complete! Thanks for choosing csvTransformer (ᵔᴥᵔ)' + '\x1b[0m\n',
+        );
+      })
+      .catch((err) => console.log(err));
+  },
+  unixToISO: (inputFile: string, indexCol: string, outputFile?: string, targetCol?: string): void => {
+    outputFile = outputFile || 'csvTransformer_output.csv';
+    targetCol = targetCol || 'date';
+    let i = 0;
+    setInterval(() => {
+      process.stdout.clearLine(0);
+      process.stdout.cursorTo(0);
+      i = (i + 1) % 4;
+      const dots = new Array(i + 1).join('.');
+      process.stdout.write('\x1b[32m' + `Working${dots}` + '\x1b[0m');
+    }, 300).unref();
+    const script = `
+import dask.dataframe as dd
+
+# Read in the CSV file
+df = dd.read_csv('${inputFile}')
+
+
+# Convert the UNIX timestamp to ISO 8601
+df.set_index('${indexCol}')
+df['${targetCol}'] = df['${targetCol}'].map_partitions(lambda x: x.astype('datetime64[ms]').dt.strftime('%Y-%m-%dT%H:%M:%SZ'))
+
+# Write the transformed dataframe to a new CSV file
+df.to_csv('${outputFile}', single_file=True, index=False)
+`;
+
+    PythonShell.runString(script, {})
+      .then(() => {
+        process.stdout.clearLine(0);
+        process.stdout.cursorTo(0);
+        process.stdout.write(
+          '\x1b[32m' + 'UNIX-->ISO Complete! Thanks for choosing csvTransformer (ᵔᴥᵔ)' + '\x1b[0m\n',
+        );
+      })
+      .catch((err) => console.log(err));
+  },
+  binaryToBoolean: (inputFile: string, targetCol: string, outputFile?: string): void => {
+    outputFile = outputFile || 'csvTransformer_output.csv';
+    let i = 0;
+    setInterval(function () {
+      process.stdout.clearLine(0);
+      process.stdout.cursorTo(0);
+      i = (i + 1) % 4;
+      const dots = new Array(i + 1).join('.');
+      process.stdout.write('\x1b[32m' + `Working${dots}` + '\x1b[0m');
+    }, 300).unref();
+    const script = `
+import dask.dataframe as dd
+
+df = dd.read_csv('${inputFile}')
+
+# convert the 'bool' column from 0 and 1 to False and True
+df['${targetCol}'] = df['${targetCol}'].map(lambda x: False if x == 0 else True).astype(str).str.lower()
+# df['${targetCol}'] = df['${targetCol}'].astype(str).str.lower()
+
+df.to_csv('${outputFile}', single_file=True, index=False)
+`;
+
+    PythonShell.runString(script, {})
+      .then(function () {
+        process.stdout.clearLine(0);
+        process.stdout.cursorTo(0);
+        process.stdout.write(
+          '\x1b[32m' + 'Binary-->Boolean Complete! Thanks for choosing csvTransformer (ᵔᴥᵔ)' + '\x1b[0m\n',
+        );
+      })
+      .catch((err) => console.error(err));
+  },
+  booleanToBinary: (inputFile: string, targetCol: string, outputFile?: string): void => {
+    outputFile = outputFile || 'csvTransformer_output.csv';
+    let i = 0;
+    setInterval(function () {
+      process.stdout.clearLine(0);
+      process.stdout.cursorTo(0);
+      i = (i + 1) % 4;
+      const dots = new Array(i + 1).join('.');
+      process.stdout.write('\x1b[32m' + `Working${dots}` + '\x1b[0m');
+    }, 300).unref();
+    const script = `
+import dask.dataframe as dd
+
+df = dd.read_csv('${inputFile}')
+
+# convert the boolean values in 'bool' column to 0 and 1
+df['${targetCol}'] = df['${targetCol}'].map(lambda x: 0 if x == False else 1)
+
+# write the modified dataframe to output.csv
+df.to_csv('${outputFile}', single_file=True, index=False)
+`;
+
+    PythonShell.runString(script, {})
+      .then(function () {
+        process.stdout.clearLine(0);
+        process.stdout.cursorTo(0);
+        process.stdout.write(
+          '\x1b[32m' + 'Binary-->Boolean Complete! Thanks for choosing csvTransformer (ᵔᴥᵔ)' + '\x1b[0m\n',
+        );
+      })
+      .catch((err) => console.error(err));
+  },
+  custom: (customScript: string): void => {
+    let i = 0;
+    setInterval(function () {
+      process.stdout.clearLine(0);
+      process.stdout.cursorTo(0);
+      i = (i + 1) % 4;
+      const dots = new Array(i + 1).join('.');
+      process.stdout.write('\x1b[32m' + `Working${dots}` + '\x1b[0m');
+    }, 300).unref();
+
+    PythonShell.runString(customScript, {})
+      .then(function () {
+        process.stdout.clearLine(0);
+        process.stdout.cursorTo(0);
+        process.stdout.write(
+          '\x1b[32m' + 'Custom Task Complete! Thanks for choosing csvTransformer (ᵔᴥᵔ)' + '\x1b[0m\n',
+        );
+      })
+      .catch((err) => console.error(err));
+  },
+  toLower: (inputFile: string, targetCol: string, outputFile?: string): void => {
+    outputFile = outputFile || 'csvTransformer_output.csv';
+    let i = 0;
+    setInterval(function () {
+      process.stdout.clearLine(0);
+      process.stdout.cursorTo(0);
+      i = (i + 1) % 4;
+      const dots = new Array(i + 1).join('.');
+      process.stdout.write('\x1b[32m' + `Working${dots}` + '\x1b[0m');
+    }, 300).unref();
+    const script = `
+import dask.dataframe as dd
+
+# Read in the CSV file
+df = dd.read_csv('${inputFile}')
+
+# Convert the 'bool' column to strings
+# Transform the first character of the 'bool' column to lower case
+df['${targetCol}'] = df['${targetCol}'].astype(str).str.lower()
+
+# Write the transformed dataframe to a new CSV file
+df.to_csv('${outputFile}', single_file=True, index=False)
+`;
+
+    PythonShell.runString(script, {})
+      .then(function () {
+        process.stdout.clearLine(0);
+        process.stdout.cursorTo(0);
+        process.stdout.write(
+          '\x1b[32m' + 'Lowercase Transform Complete! Thanks for choosing csvTransformer (ᵔᴥᵔ)' + '\x1b[0m\n',
+        );
+      })
+      .catch((err) => console.error(err));
+  },
+  merge: (inputFolder: string, targetCol: string, outputFile?: string): void => {
+    inputFolder = inputFolder || 'files/*.csv';
+    outputFile = outputFile || 'csvTransformer_output.csv';
+    let i = 0;
+    setInterval(function () {
+      process.stdout.clearLine(0);
+      process.stdout.cursorTo(0);
+      i = (i + 1) % 4;
+      const dots = new Array(i + 1).join('.');
+      process.stdout.write('\x1b[32m' + `Merging${dots}` + '\x1b[0m');
+    }, 300).unref();
+    const script = `
+import dask.dataframe as dd
+import glob
+
+#read in the csv files
+csv_files = glob.glob('${inputFolder}')
+dfs = [dd.read_csv(f) for f in csv_files]
+
+#merge the dataframes on the common column (id)
+merged_df = dfs[0]
+for df in dfs[1:]:
+merged_df = dd.merge(merged_df, df, on='${targetCol}')
+merged_df = merged_df.set_index('${targetCol}')
+
+#write the merged dataframe to a new csv file
+merged_df.to_csv('${outputFile}', single_file=True)
+`;
+
+    PythonShell.runString(script, {})
+      .then(function () {
+        process.stdout.clearLine(0);
+        process.stdout.cursorTo(0);
+        process.stdout.write('\x1b[32m' + 'Merge Complete! Thanks for choosing csvTransformer (ᵔᴥᵔ)' + '\x1b[0m\n');
+      })
+      .catch((err) => console.error(err));
+  },
+  aggregatorInt: (): void => {
+    let i = 0;
+    setInterval(function () {
+      process.stdout.clearLine(0);
+      process.stdout.cursorTo(0);
+      i = (i + 1) % 4;
+      const dots = new Array(i + 1).join('.');
+      process.stdout.write('\x1b[32m' + `Working${dots}` + '\x1b[0m');
+    }, 300).unref();
+    const script = `
+#refactored
+
+import pandas as pd
+
+#read csv file
+df = pd.read_csv('input.csv')
+
+#aggregate the number of ratings 1-5 in the 'rating' column of each unique 'id'
+df_agg = df.groupby('id')['rating'].value_counts().unstack().fillna(0).round(0).astype(int)
+
+#write the count of ratings 1-5 to 5 new columns
+df_agg.rename(columns={1:'rating1', 2:'rating2', 3:'rating3', 4:'rating4', 5:'rating5'}, inplace=True)
+
+#output the file
+df_agg.to_csv('output.csv')
+`;
+
+    PythonShell.runString(script, {})
+      .then(function () {
+        process.stdout.clearLine(0);
+        process.stdout.cursorTo(0);
+        process.stdout.write(
+          '\x1b[32m' + 'Ratings Aggregation Complete! Thanks for choosing csvTransformer (ᵔᴥᵔ)' + '\x1b[0m\n',
+        );
+      })
+      .catch((err) => console.error(err));
+  },
+  generator: async (
+    outputFile: string,
+    headers: string,
+    ...valueCols: ((...args: any[]) => unknown)[]
+  ): Promise<void> => {
+    const writeStream = fs.createWriteStream('input.csv');
+    writeStream.write(`${headers}\n`);
+    for (let i = 1; i <= 100; i++) {
+      const overWatermark = writeStream.write(`${valueCols.map((valueCol) => valueCol()).join(',')}\n`);
+
+      if (!overWatermark) {
+        await new Promise((resolve) => writeStream.once('drain', resolve)).then(() => {
+          fs.stat('input.csv', (err: any, stats: any) => {
+            if (err) {
+              console.error(err);
+            } else {
+              process.stdout.write('\x1b[32m' + `${stats.size} bytes written` + '\x1b[0m\r');
+            }
+          });
+        });
+      }
+    }
+
+    writeStream.on('finish', () => {
+      fs.stat('input.csv', (err: any, stats: any) => {
+        if (err) {
+          console.error(err);
+        } else {
+          process.stdout.write(
+            '\x1b[32m' + `CSV file successfully created. Size: ${stats.size / 1000000} MB (ᵔᴥᵔ)` + '\x1b[0m\n',
+          );
+        }
+      });
+    });
+    writeStream.end();
+  },
+};
